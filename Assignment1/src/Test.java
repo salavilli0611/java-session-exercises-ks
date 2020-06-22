a4a46ca71579059bf89a8f1bd2cc0e9f8f89e5d7
salavilli <salavilli@USSCASALAVILL13.us.deloitte.com>
2020-06-22 08:01:03 -0700

    Checking into the branch for git testing

---
+++ Assignment1/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("My Git Check in");
+	}
+
+}
